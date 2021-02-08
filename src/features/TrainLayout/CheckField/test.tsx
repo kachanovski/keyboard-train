02d4d256e5e0073bfd2df2807a936b4bde1fbf58
Denis <den-282008@yandex.ru>
2021-02-09 00:28:06 +0300

    Refactor test.tsx

--- src/features/TrainLayout/CheckField/test.tsx
+++ src/features/TrainLayout/CheckField/test.tsx
@@ -1,44 +1,49 @@
-import React, { useState } from 'react'
+import React, {ChangeEvent, useState} from 'react'
+
+export type StrType = {
+	id: number
+	title: string
+}
 
 export const AppTS = () => {
-	const [str, setStr] = useState<any[]>([
-		{ id: 1, title: 'Hello world' },
-		{ id: 2, title: '!' },
-		{ id: 3, title: '!!' },
-		{ id: 4, title: '1314 24' },
-		{ id: 5, title: '23525' },
+	const [str, setStr] = useState<StrType[]>([
+		{id: 1, title: "Hello world"},
+		{id: 2, title: "!"},
+		{id: 3, title: "!!"},
+		{id: 4, title: "1314 24"},
+		{id: 5, title: "23525"}
 	])
 
-	const [count, setCount] = useState(0)
 
-	let [string, setString] = useState<string>('')
-	const [inpValue, setInpValue] = useState('')
-
-	function schet() {
-		setString(str[count].title)
-		if (count == str.length - 1) {
-			alert('yoyoyo')
-		} else {
-			setCount(count + 1)
+	let [strUser, setStrUser] = useState<string>('')
+	let [count, setCount] = useState<number>(0)
+	let [string, setString] = useState<string>(str[count].title)
+	const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+		setStrUser(e.currentTarget.value)
+		if (str[count].title.length - 1 === strUser.length) {
+			counter()
+			setStrUser('')
+			setString(str[count].title)
 		}
+
 	}
 
-	const onChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setInpValue(e.currentTarget.value)
-		setString(str[count].title)
-
-		if (inpValue.length == str[count].title.length - 1) {
-			setInpValue('')
-			setCount(count + 1)
-			schet()
+	function counter () {
+		if(str.length - 1 === count) {
+			alert("yo")
+		} else {
+			setCount(count = count + 1)
 		}
 	}
 
 	return (
-		<div className={'centr'}>
+		<div className={"centr"}>
 			<span>{string}</span>
-			<input type={'text'} value={inpValue} onChange={onChangeHandler} />
-			<button onClick={schet}>-</button>
+			<input type={"text"}
+				   value={strUser}
+				   onChange={onChangeHandler}
+			/>
+
 		</div>
-	)
+	);
 }
