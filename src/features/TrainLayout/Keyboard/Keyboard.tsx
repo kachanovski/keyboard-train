2935e63c5414ae6b1bd9f24b63ceb5f93133cb0c
Denis <den-282008@yandex.ru>
2021-02-11 02:58:27 +0300

    Refactor props keyboard.tsx

--- src/features/TrainLayout/Keyboard/Keyboard.tsx
+++ src/features/TrainLayout/Keyboard/Keyboard.tsx
@@ -9,41 +9,38 @@ type KeyBoardPropsType = {
 }
 export const KeyBoard = (props: KeyBoardPropsType) => {
 
-    const onClickButton = (code: any) => {
+    const onClickButton = (code: number) => {
         console.log(code)
     }
-
-    console.log(props.result[props.keyCount])
-    console.log(props.result)
 
     return (
         <div>
             <div className="keyboard-row">
-                {keySets.en[0].map((item) => <Button result={props.result} count={props.keyCount} value={item.glyph}
+                {keySets.en[0].map((item) => <Button result={props.result} keyCount={props.keyCount} value={item.glyph}
                                                      onClickButton={onClickButton}
                                                      upperValue={item.upperGlyph} colorValue={item.color}
                                                      sizeValue={item.size} code={item.code}/>)}
             </div>
             <div className="keyboard-row">
-                {keySets.en[1].map((item) => <Button result={props.result} count={props.keyCount} value={item.glyph}
+                {keySets.en[1].map((item) => <Button result={props.result} keyCount={props.keyCount} value={item.glyph}
                                                      onClickButton={onClickButton}
                                                      upperValue={item.upperGlyph} colorValue={item.color}
                                                      sizeValue={item.size} code={item.code}/>)}
             </div>
             <div className="keyboard-row">
-                {keySets.en[2].map((item) => <Button result={props.result} count={props.keyCount} value={item.glyph}
+                {keySets.en[2].map((item) => <Button result={props.result} keyCount={props.keyCount} value={item.glyph}
                                                      onClickButton={onClickButton}
                                                      upperValue={item.upperGlyph} colorValue={item.color}
                                                      sizeValue={item.size} code={item.code}/>)}
             </div>
             <div className="keyboard-row">
-                {keySets.en[3].map((item) => <Button result={props.result} count={props.keyCount} value={item.glyph}
+                {keySets.en[3].map((item) => <Button result={props.result} keyCount={props.keyCount} value={item.glyph}
                                                      onClickButton={onClickButton}
                                                      upperValue={item.upperGlyph} colorValue={item.color}
                                                      sizeValue={item.size} code={item.code}/>)}
             </div>
             <div className="keyboard-row">
-                {keySets.en[4].map((item) => <Button result={props.result} count={props.keyCount}  value={item.glyph}
+                {keySets.en[4].map((item) => <Button result={props.result} keyCount={props.keyCount} value={item.glyph}
                                                      onClickButton={onClickButton}
                                                      upperValue={item.upperGlyph} colorValue={item.color}
                                                      sizeValue={item.size} code={item.code}/>)}
@@ -52,14 +49,25 @@ export const KeyBoard = (props: KeyBoardPropsType) => {
     )
 }
 
-export const Button = (props: any) => {
+type ButtonPropsType = {
+    keyCount: number
+    result: Array<string>
+    value: string
+    onClickButton: (code: number) => void
+    upperValue: string
+    colorValue: string
+    sizeValue: string
+    code: number
+}
+
+export const Button = (props: ButtonPropsType) => {
 
     const onClick = () => {
         props.onClickButton(props.code)
     }
 
     return (
-        <div onClick={onClick} className={props.result[props.count] === props.value || props.result[props.count] === props.upperValue ? "key-active" : "key"}>
+        <div onClick={onClick} className={props.result[props.keyCount] === props.value || props.result[props.keyCount] === props.upperValue ? "key-active" : "key"}>
             <span>{props.value}</span>
 
         </div>
