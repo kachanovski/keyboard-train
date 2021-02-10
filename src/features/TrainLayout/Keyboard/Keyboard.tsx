98ec21b51ba2823e261029087592151023ee7c42
Denis <den-282008@yandex.ru>
2021-02-10 22:39:21 +0300

    Refactor keyboard.tsx

--- src/features/TrainLayout/Keyboard/Keyboard.tsx
+++ src/features/TrainLayout/Keyboard/Keyboard.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import { keySets } from '../../../accets/KeysArrays';
+import React, {ChangeEvent, useState} from 'react';
+import {keySets} from '../../../accets/KeysArrays';
 import './../../../App.css';
+import {Login} from "../../Auth/Login";
+
 
 export const KeyBoard = () => {
 
@@ -8,22 +10,48 @@ export const KeyBoard = () => {
         console.log(code)
     }
 
+    const [text, setText] = useState<Array<string>>([])
+    const [count, setCount] = useState(0)
+    const [string, setString] = useState('')
+    const onChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        const lastSymbol = e.currentTarget.value.slice(-1)
+        setCount(count + 1)
+        setText([...text, lastSymbol])
+        setString(text.join(""))
+    }
+
     return (
         <div>
+            <input value={string} onChange={onChangeHandler}/>
             <div className="keyboard-row">
-                {keySets.en[0].map((item) => <Button value={item.glyph} onClickButton={onClickButton} upperValue={item.upperGlyph} colorValue={item.color} sizeValue={item.size} code={item.code} />)}
+                {keySets.en[0].map((item) => <Button count={count} text={text} value={item.glyph}
+                                                     onClickButton={onClickButton}
+                                                     upperValue={item.upperGlyph} colorValue={item.color}
+                                                     sizeValue={item.size} code={item.code}/>)}
             </div>
             <div className="keyboard-row">
-                {keySets.en[1].map((item) => <Button value={item.glyph} onClickButton={onClickButton} upperValue={item.upperGlyph} colorValue={item.color} sizeValue={item.size} code={item.code} />)}
+                {keySets.en[1].map((item) => <Button count={count} text={text} value={item.glyph}
+                                                     onClickButton={onClickButton}
+                                                     upperValue={item.upperGlyph} colorValue={item.color}
+                                                     sizeValue={item.size} code={item.code}/>)}
             </div>
             <div className="keyboard-row">
-                {keySets.en[2].map((item) => <Button value={item.glyph} onClickButton={onClickButton} upperValue={item.upperGlyph} colorValue={item.color} sizeValue={item.size} code={item.code} />)}
+                {keySets.en[2].map((item) => <Button count={count} text={text} value={item.glyph}
+                                                     onClickButton={onClickButton}
+                                                     upperValue={item.upperGlyph} colorValue={item.color}
+                                                     sizeValue={item.size} code={item.code}/>)}
             </div>
             <div className="keyboard-row">
-                {keySets.en[3].map((item) => <Button value={item.glyph} onClickButton={onClickButton} upperValue={item.upperGlyph} colorValue={item.color} sizeValue={item.size} code={item.code} />)}
+                {keySets.en[3].map((item) => <Button count={count} text={text} value={item.glyph}
+                                                     onClickButton={onClickButton}
+                                                     upperValue={item.upperGlyph} colorValue={item.color}
+                                                     sizeValue={item.size} code={item.code}/>)}
             </div>
             <div className="keyboard-row">
-                {keySets.en[4].map((item) => <Button value={item.glyph} onClickButton={onClickButton} upperValue={item.upperGlyph} colorValue={item.color} sizeValue={item.size} code={item.code} />)}
+                {keySets.en[4].map((item) => <Button count={count} text={text} value={item.glyph}
+                                                     onClickButton={onClickButton}
+                                                     upperValue={item.upperGlyph} colorValue={item.color}
+                                                     sizeValue={item.size} code={item.code}/>)}
             </div>
         </div>
     )
@@ -36,8 +64,8 @@ export const Button = (props: any) => {
     }
 
     return (
-        <div onClick={onClick} className="key">
-            <span >{props.value}</span>
+        <div onClick={onClick} className={props.text[props.count - 1] === props.value ? "key-active" : "key"}>
+            <span>{props.value}</span>
 
         </div>
     )
