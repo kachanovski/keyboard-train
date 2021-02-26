6510e031eae651fdced8a8cc4531ec7a66be4e81
vzanimonets <vzanimonets@gmail.com>
2021-02-26 11:12:18 +0300

    moving displayValue function to useEffect hook & fix typescript errors

--- src/features/TrainLayout/Results/Results.tsx
+++ src/features/TrainLayout/Results/Results.tsx
@@ -1,34 +1,36 @@
-import React, { useEffect, useState } from 'react'
+import React, { Dispatch, useEffect, useState } from 'react'
 import s from './Results.module.css'
+
 
 type TimerPropsType = {
 	isActiveTimer: boolean
-	setSeconds: (sec: any) => number
+	setSeconds: Dispatch<(prevState: number) => number>
 	mistakes: number
 	seconds: number
 }
 
-export const Results = (props: any) => {
+export const Results: React.FC<TimerPropsType> = ({ isActiveTimer, setSeconds, mistakes, seconds }) => {
 
 	const [displayTime, setDisplayTime] = useState<{ m: string, s: string }>({ m: '00', s: '00' })
 	useEffect(() => {
 		let id = setInterval(() => {
-			if (props.isActiveTimer) {
-				props.setSeconds((sec: number) => sec + 1)
+			if (isActiveTimer) {
+				setSeconds((sec: number) => sec + 1)
 			}
 		}, 1000)
 		return () => clearInterval(id)
-	}, [props.isActiveTimer])
+	}, [isActiveTimer, setSeconds])
 
 	useEffect(() => {
-		displayValue(props.seconds)
-	}, [props.seconds])
+		const displayValue = (seconds: number) => {
+			const displaySeconds = (seconds % 60).toString().padStart(2, '0');
+			const displayMinutes = Math.floor(seconds / 60).toString().padStart(2, '0');
+			setDisplayTime(d => ({ ...d, m: displayMinutes, s: displaySeconds }))
+		}
+		displayValue(seconds)
+	}, [seconds])
 
-	function displayValue(seconds: number) {
-		const displaySeconds = (seconds % 60).toString().padStart(2, '0');
-		const displayMinutes = Math.floor(seconds / 60).toString().padStart(2, '0');
-		setDisplayTime({ ...displayTime, m: displayMinutes, s: displaySeconds })
-	}
+
 
 	return (
 		<div className={s.results}>
@@ -37,7 +39,7 @@ export const Results = (props: any) => {
 					<span>Время: {displayTime.m}:{displayTime.s}</span>
 				</div>
 				<div className={s.title}>
-					<span>Ошибок: {props.mistakes}</span>
+					<span>Ошибок: {mistakes}</span>
 				</div>
 			</div>
 		</div>
